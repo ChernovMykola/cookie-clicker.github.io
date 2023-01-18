22f9240c3ec5c181b26922476802f68ee41e5f5a
ChernovMykola <4ernovnikola@gmail.com>
2023-01-18 10:58:30 +0100

    AddDisable

--- script.js
+++ script.js
@@ -1,44 +1,63 @@
 var cookies = 0;
-var cpc = 1;
 var multiplier = 1;
 var price = 20;
 var autoPrice = 200;
 var autoClickVal = 0;
 var time = 30;
 var bonusPrice = 200;
-var multButton = document.querySelector('#buttonMult');
 
-// multButton.setAttribute('disabled', true);
+examMult()
+examAuto()
+examBonus()
 
-cookies.value = function butM(){
+function bakeCookie(){
+  cookies += multiplier;
+  document.getElementById('cookies-number').innerHTML = "Cookies: " + cookies;
+  examMult()
+  examAuto()
+  examBonus()
+}
+
+function examMult(){
   if (cookies < price) {
-    multButtom.setAttribute('disabled', true);
-  }else{
-    multButtom.removeAttribute('disabled');
+  document.getElementById('buttonMult').disabled = true;
+  }else if(cookies >= price) {
+  document.getElementById('buttonMult').disabled = false;
+}
+}
+
+function examAuto(){
+  if (cookies < autoPrice) {
+    document.getElementById('autoClick').disabled = true;
+  }else if (cookies >= autoPrice) {
+    document.getElementById('autoClick').disabled = false;
   }
 }
-function bakeCookie(){
-  cookies += cpc;
-  document.getElementById('cookies-number').innerHTML = "Cookies: " + cookies;
+
+function examBonus(){
+  if (cookies < bonusPrice) {
+    document.getElementById('bonusClick').disabled = true;
+  }else if (cookies >= bonusPrice) {
+    document.getElementById('bonusClick').disabled = false;
+  }
 }
+
 
 function hireBaker() {
   if (cookies >= price) {
     if (secondsLeft === 30) {
-      cpc += 1;
       multiplier += 1;
     }else if (secondsLeft < 30) {
-      cpc += 2;
       multiplier += 2;
     }
     cookies -= price;
-
     price *= 2;
     document.getElementById('multiplier').innerHTML = "Multiplier x: " + multiplier;
     document.getElementById('cookies-number').innerHTML = "Cookies: " + cookies;
     document.getElementById('price').innerHTML = "You need for upgrade your multiplier: " + price;
-  }else {
-    alert('You need more cookies')
+    examMult()
+    examAuto()
+    examBonus()
   }
 }
 
@@ -55,8 +74,9 @@ function autoCookie(){
     autoPrice *= 1.5;
     autoClickVal += 1;
     document.getElementById('autoPrice').innerHTML = "You need for buy auto-cliker: " + autoPrice;
-  }else {
-    alert('You need more cookies')
+    examMult()
+    examAuto()
+    examBonus()
   }
 }
 
@@ -67,20 +87,19 @@ function bonus2() {
   if (cookies >= bonusPrice) {
     cookies -= bonusPrice;
     multiplier *= 2;
-    cpc *= 2;
     document.getElementById('multiplier').innerHTML = "Multiplier x: " + multiplier;
-    alert('bonus enabled')
+    // alert('bonus enabled')
     timerOn = true;
     startTimer()
     setTimeout(() => {
       multiplier *= 0.5;
-      cpc *= 0.5;
       document.getElementById('multiplier').innerHTML = "Multiplier x: " + multiplier;
-      alert('bonus disabled');
+      // alert('bonus disabled');
       timerOn = false;
     }, secondsLeft * 1000)
-  }else {
-    alert('You need more cookies')
+    examMult()
+    examAuto()
+    examBonus()
   }
   }
 
@@ -99,15 +118,15 @@ function startTimer() {
       }, 1000);
 }
 
-multButtom.setAttribute('disabled', true);
+// multButtom.setAttribute('disabled', true);
 
-cookies.value = function butM(){
-  if (cookies < price) {
-    multButton.setAttribute('disabled', true);
-  }else{
-    multButton.removeAttribute('disabled');
-  }
-}
+//  = function butM(){
+//   if (cookies < price) {
+//     multButton.setAttribute('disabled', true);
+//   }else{
+//     multButton.removeAttribute('disabled');
+//   }
+// }
 
 // function bonus2() {
 //   multiplier *= 2;
